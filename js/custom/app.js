1c8ed757dff7d0e4220a47ec729b8cd3e9986c53
Timilehin Aderinola <timilehin.aderinola@andela.co>
2015-01-16 10:24:25 +0100

    Linked to db

--- js/custom/app.js
+++ js/custom/app.js
@@ -2,14 +2,30 @@ var bibleApp = angular.module('bibleApp', []);
 
 bibleApp.controller('bibleController', ['$scope', '$http', function($scope, $http) {
   console.log("working...");
-    $http.get('../../data/testUser.json').success(function (data){
-    console.log(data.fname + " " + data.lname);
-    console.log(data);
-    $scope.username = data.login.email;
-    console.log($scope.username);
-    $scope.questions = data.questions;
-    $scope.joined = data.joined;
+    
+    $http.get('http://localhost:5000/api/questions').success(function (data){
+      $scope.questions = data;
+
+      // $scope.username = data.login.email;
+      // console.log($scope.username);
+
   });
 
+  $scope.showAnswers = function (question_id) {
+    var arr = [];
+    for(i=0; i<$scope.questions.length; i++) {
+      $scope.count = i;
+      if($scope.questions[i].answers > 0) {
+        $http.get('http://localhost:5000/api/questions/' + $scope.questions[i]._id + '/answers')
+          .success(function (data) {
+            arr.push(data[0]);
+            console.log('Within the loop', arr);
+          });
+      }
+    };
+    $scope.answers = arr;
+    console.log('Final array', $scope.answers);
+    $(".answer").slideToggle(500);
+  };
   
 }]);
